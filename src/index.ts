60dd7c232a0b422807a9485bb3750d369a8bfe00
Ver0018 <rogerpantilyowger@gmail.com>
2025-07-02 01:54:21 +0800

    added base configs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
+import cors from "cors"
+import compression from "compression"
 import express from "express"
+import helmet from "helmet"
+
+import { connectDatabase } from "./services/mongoose"
+
+const PORT = process.env.PORT || 3000
+
+const whitelist = ["http://localhost:3000"]
+
+const corsOptions: cors.CorsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error(`Origin '${origin}' not allowed by CORS`))
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+}
+
+connectDatabase()
 
 const app = express()
 
-app.listen(3000, () => console.log("Server running on port 3000"))
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(express.json({ limit: "2mb" }))
+app.use(
+    express.urlencoded({ limit: "1mb", extended: true, parameterLimit: 5000 })
+)
+app.use(compression())
+
+app.listen(PORT, () => {
+    return console.log(`Server running on port ${PORT}`)
+})
+
+process.on("unhandledRejection", (error) => {
+    console.log("unhandledRejection", error)
+})
+
+process.on("uncaughtException", (error) => {
+    console.log("uncaughtException", error)
+})
---
+++ src/services/mongoose.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+export async function connectDatabase() {
+    mongoose.connect(process.env.MONGO_URI)
+
+    mongoose.connection.on("connected", () => {
+        console.log("Connected to MongoDB")
+    })
+
+    mongoose.connection.on("error", (error) => {
+        console.log("Error connecting to MongoDB", error)
+    })
+
+    mongoose.connection.on("disconnected", () => {
+        console.log("Disconnected from MongoDB")
+    })
+}
