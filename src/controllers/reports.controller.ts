0c8ffa02a0f48415ba701ea78e945052b238a455
Ver0018 <rogerpantilyowger@gmail.com>
2025-07-02 21:35:51 +0800

    added create post controller

--- src/controllers/reports.controller.ts
+++ src/controllers/reports.controller.ts
@@ -1,6 +1,29 @@
 import { Request, Response } from "express"
+import { z } from "zod"
 
 import { createReport } from "src/services/mongoose/reports.service"
+import { validator } from "src/utils/validator"
+
+export const verifiedInfoSchema = z.object({
+    verifiedBy: z.string().min(1),
+    verifiedAt: z.coerce.date(),
+})
+
+export const createReportSchema = z.object({
+    type: z.string().min(1, "Report type is required"),
+    message: z.string().optional(),
+    lat: z.number(),
+    lng: z.number(),
+    imageUrls: z.array(z.string().url()).optional(),
+    authorId: z.string().min(1, "Author ID is required"),
+    barangayId: z.string().optional(),
+    status: z.enum(["pending", "verified", "rejected", "archived"]).optional(),
+    verifiedInfo: verifiedInfoSchema.optional(),
+})
+
+const { getBody } = validator({
+    body: createReportSchema,
+})
 
 export async function createReportHandler(req: Request, res: Response) {
     const {
@@ -13,7 +36,16 @@ export async function createReportHandler(req: Request, res: Response) {
         barangayId,
         status,
         verifiedInfo,
-    } = req.body
+    } = getBody(req)
+
+    let sanitizedVerifiedInfo = undefined
+
+    if (verifiedInfo?.verifiedAt && verifiedInfo?.verifiedBy) {
+        sanitizedVerifiedInfo = {
+            verifiedBy: verifiedInfo.verifiedBy,
+            verifiedAt: verifiedInfo.verifiedAt,
+        }
+    }
 
     const report = await createReport({
         type,
@@ -24,7 +56,7 @@ export async function createReportHandler(req: Request, res: Response) {
         authorId,
         barangayId,
         status,
-        verifiedInfo,
+        verifiedInfo: sanitizedVerifiedInfo,
     })
 
     res.status(201).json({ report })
